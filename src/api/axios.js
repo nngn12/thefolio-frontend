cd5736a074cae221a266b539176b17f790c4db5b
nngn12 <analeahbasco1@gmail.com>
2026-04-18 21:13:52 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,15 @@
-// frontend/src/api/axios.js
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    // Use env variable in production, fallback to localhost for dev
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 instance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
-}); export default
-    instance;
+});
+
+export default instance;
